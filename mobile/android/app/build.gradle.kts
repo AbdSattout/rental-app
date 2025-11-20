3e510448fe9fc8f09a0a7389043fd9f8c773c516
Abd Sattout <abdsat@duck.com>
2025-11-20 18:53:34 +0300

    migrate to codemagic

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -38,18 +38,18 @@ android {
         versionName = flutter.versionName
     }
 
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("release")
-        }
-    }
-
     signingConfigs {
         create("release") {
             keyAlias = keystoreProperties["keyAlias"] as String
             keyPassword = keystoreProperties["keyPassword"] as String
             storeFile = keystoreProperties["storeFile"]?.let { file(it) }
             storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
