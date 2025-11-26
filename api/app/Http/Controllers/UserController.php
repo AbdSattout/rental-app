bc61540e3b2b146edc04a1d6aab5f97af1239c2c
Hadi <abdelhadialkadri@gmail.com>
2025-11-26 15:20:32 +0300

    first commit

---
+++ api/app/Http/Controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request){
+       $request->validate([
+        'phone_number'=>'required|string|max:10',
+        'password'=>'required|string|min:8|confirmed'
+       ]);
+       $user = User::create([
+        'phone_number'=>$request->phone_number,
+        'password'=>Hash::make($request->password)
+       ]);
+       $token = $user->createToken('auth_token')->plainTextToken;
+       return response()->json([
+        'message'=>'user register successfully',
+        'data'=>$user ,
+        'token'=>$token,
+        'status'=>201
+       ]);
+    }
+    public function login(Request $request){
+        $request->validate([
+              'phone_number'=>'string',
+        'password'=>'string|'
+        ]);
+        if(!Auth::attempt($request->only('phone_number' , 'password')))
+            return response()->json(['message'=>'invalid phone number or password']);
+        $user = User::where('phone_number' , $request->phone_number)->FirstOrFail();
+        $user->tokens()->delete();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+        'message'=>'login successed' ,
+      'user'=>$user ,
+      'token'=>$token] ,200);
+
+    }
+    public function logout(Request $request){
+       $request->user()->currentAccessToken()->delete();
+       return response()->json([
+        'message'=>'user deleted successfully',
+        'status'=>200
+       ]);
+    }
+}
--- api/database/migrations/0001_01_01_000000_create_users_table.php
+++ api/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,16 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+           $table->string('phone_number')->unique();
             $table->string('password');
+            $table->string('role')->default('tenant');
+            $table->boolean('be_host')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('phone_number')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
