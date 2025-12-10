b5fef787772e109f1f8a721f6fd722f6fbb370d5
Abd Sattout <abdsat@duck.com>
2025-12-10 15:08:16 +0300

    fix merge

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         "phone_number",
         "password",
+        "is_approved",
         "role",
         "requesting_host",
     ];
