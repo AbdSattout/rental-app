5cd8f5b920bd76b8229af9c05f420a4a90b9b8ec
Ghai8th <ghai8thnm@gmail.com>
2026-01-03 18:52:07 +0300

    fixing errors

--- api/app/Http/Controllers/MessageController.php
+++ api/app/Http/Controllers/MessageController.php
@@ -154,5 +154,23 @@ class MessageController extends Controller
         return response()->json(['message' => 'Read updated']);
     }
 
+    private function calculateStatus($message, $conversationUsers)
+    {
+        $hasRead = false;
+        $hasDelivered = false;
+
+        foreach ($conversationUsers as $user) {
+            if ($user->id === $message->sender_id) continue;
+
+            if ($user->pivot->last_read_message_id && $user->pivot->last_read_message_id >= $message->id) {
+                $hasRead = true;
+            }
+            if ($user->pivot->last_delivered_message_id && $user->pivot->last_delivered_message_id >= $message->id) {
+                $hasDelivered = true;
+            }
+        }
+
+        return $hasRead ? 'read' : ($hasDelivered ? 'delivered' : 'sent');
+    }
 
 }
