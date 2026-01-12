efb30273e34149a2aafcb015d26d8d1073496b8f
Abd Sattout <abdsat@duck.com>
2026-01-12 16:47:03 +0300

    fix chip colors

--- mobile/lib/presentation/widgets/category_button.dart
+++ mobile/lib/presentation/widgets/category_button.dart
@@ -19,22 +19,27 @@ class CategoryButton extends StatefulWidget {
 class _CategoryButtonState extends State<CategoryButton> {
   @override
   Widget build(BuildContext context) {
-    final primaryColor = Theme.of(context).colorScheme.primary;
-    final secondaryColor = Theme.of(context).colorScheme.onSecondaryContainer;
+    final primaryColor = ColorScheme.of(context).primary;
+    final secondaryColor = ColorScheme.of(context).onSecondaryContainer;
+    final bgColor = ColorScheme.of(context).brightness == .light
+        ? Colors.white
+        : ColorScheme.of(context).surfaceBright;
 
     return GestureDetector(
       onTap: widget.onTap,
       child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        padding: .symmetric(horizontal: 15, vertical: 10),
         decoration: BoxDecoration(
-          color: widget.isSelected ? primaryColor : Colors.white,
+          color: widget.isSelected ? primaryColor : bgColor,
           borderRadius: BorderRadius.circular(10),
         ),
         child: Center(
           child: Text(
             widget.title,
             style: TextTheme.of(context).titleSmall!.copyWith(
-              color: widget.isSelected ? Colors.white : secondaryColor,
+              color: widget.isSelected
+                  ? ColorScheme.of(context).onPrimary
+                  : secondaryColor,
               fontSize: 13,
             ),
           ),
